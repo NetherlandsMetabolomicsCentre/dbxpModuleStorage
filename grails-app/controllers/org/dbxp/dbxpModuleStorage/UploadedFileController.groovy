9cc742b9b45f49ca14ca7af7c600fecebd842768
4np <work@osx.eu>
2011-08-01 14:36:07 +0000

    - improved download logic

--- grails-app/controllers/org/dbxp/dbxpModuleStorage/UploadedFileController.groovy
+++ grails-app/controllers/org/dbxp/dbxpModuleStorage/UploadedFileController.groovy
@@ -4,6 +4,8 @@ import com.mongodb.gridfs.GridFSFile
 import grails.converters.JSON
 
 class UploadedFileController {
+	static final int BUFF_SIZE = 100000;
+	static final byte[] buffer = new byte[BUFF_SIZE];
 
     def uploadedFileService
 
@@ -16,7 +18,6 @@ class UploadedFileController {
 
         // render info about stored uploaded file
         render([fileName: uploadedFile.fileName, fileSize: uploadedFile.fileSize, fileId: uploadedFile.id] as JSON)
-
     }
 
     def deleteUploadedFile = {
@@ -36,17 +37,44 @@ class UploadedFileController {
     }
 
     def downloadUploadedFile = {
-
         UploadedFile uploadedFile = UploadedFile.get(params.fileId)
         if (!uploadedFile) response.sendError(404, "No uploaded file could be found matching id: ${params.fileId}.")
 
         GridFSFile gridFSFile = uploadedFileService.getGridFSDBFileByID(uploadedFile.gridFSFile_id)
         if (!gridFSFile) response.sendError(404, "No file attached to UploadedFile")
 
-        response.setHeader("Content-disposition", "attachment;filename=\"${uploadedFile.fileName}\"")
+		response.setStatus(200)
+		response.setHeader("Content-disposition", "attachment; filename=\"${uploadedFile.fileName}\"")
 		response.setContentType("application/octet-stream")
-        response.setContentLength(uploadedFile.fileSize as int)
-        response.outputStream << gridFSFile.inputStream
-        response.outputStream.close()
+		response.setContentLength(uploadedFile.fileSize as int)
+
+		// define input and output streams
+		InputStream inStream = null
+		OutputStream outStream = null
+
+		// handle file download
+		try {
+			inStream = gridFSFile.inputStream
+			outStream = response.getOutputStream()
+
+			while (true) {
+				synchronized (buffer) {
+					int amountRead = inStream.read(buffer)
+					if (amountRead == -1) {
+						break
+					}
+					outStream.write(buffer, 0, amountRead)
+				}
+				outStream.flush()
+			}
+		} catch (Exception e) {
+			// whoops, looks like something went wrong
+			println "download failed! ${e.getMessage()}"
+		} finally {
+			if (inStream != null) inStream.close()
+			if (outStream != null) outStream.close()
+		}
+
+		return false
     }
 }
