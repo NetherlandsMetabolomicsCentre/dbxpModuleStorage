92fa7e62a506bb008d3965bc81507369bfaf4807
4np <work@osx.eu>
2011-09-06 15:21:11 +0000

    - added rating (not completely done) - moved determineAmountOfSamplesWithData to uploadedFile domain class

--- grails-app/domain/org/dbxp/dbxpModuleStorage/UploadedFile.groovy
+++ grails-app/domain/org/dbxp/dbxpModuleStorage/UploadedFile.groovy
@@ -58,4 +58,41 @@ class UploadedFile {
     void delete() {
         uploadedFileService.deleteUploadedFile(this)
     }
+
+	/**
+	 * Rating calculation:
+	 * 0 = file is uploaded (exists)
+	 * 1 = file is attached to a study
+	 * 2 = file is attached to assays
+	 * 3 = all samples are recognized
+	 * 4 = all features are recognized
+	 * 5 = study is public
+	 */
+	def getRating() {
+		def maxPoints = 5
+		def points = 0
+
+		// 1 : file is attached to a study
+		// 2 : file is attach to a(n) assay(s)
+		points = (this.assay) ? 2 : 0
+
+		// 3 : all samples are recognized
+		points = (points == 2 && this.parsedFile && this.assay?.samples.count() == this.determineAmountOfSamplesWithData()) ? 3 : 2
+
+		// 4 : all features are recognized
+		// TODO
+
+		// 5 : study is public
+		// TODO
+
+		// calculate and return rating
+		return (points / maxPoints)
+	}
+
+	def determineAmountOfSamplesWithData() {
+		def sampleNamesInFile = parsedFileService.getSampleNames(this?.parsedFile)
+		def samplesInAssay = this?.assay ? this.assay.samples : []
+		def sampleNamesInAssay = samplesInAssay*.name
+		sampleNamesInAssay.intersect(sampleNamesInFile).size()
+	}
 }
