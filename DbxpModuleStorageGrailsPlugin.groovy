16c3b5c0cb5902568e117fef13975af1a7306404
michaelvanvliet <info@michaelvanvliet.nl>
2012-01-16 09:00:17 +0000

    Reverted some changes

--- DbxpModuleStorageGrailsPlugin.groovy
+++ DbxpModuleStorageGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class DbxpModuleStorageGrailsPlugin {
     // the plugin version
-    def version = "0.2.6.3"
+    def version = "0.2.6.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
